b9883afa6b38f7f3a6744e4270c56e2503b97ad1
YUVA-2329 <yuvakishore.vps@gmail.com>
2026-08-07 17:41:46 +0530

    Fix SMAAEffect types

--- src/components/Hyperspeed.tsx
+++ src/components/Hyperspeed.tsx
@@ -475,9 +475,7 @@ const Hyperspeed = ({ effectOptions = DEFAULT_EFFECT_OPTIONS }: any) => {
         const smaaPass = new EffectPass(
           this.camera,
           new SMAAEffect({
-            preset: SMAAPreset.MEDIUM,
-            searchImage: SMAAEffect.searchImageDataURL,
-            areaImage: SMAAEffect.areaImageDataURL
+            preset: SMAAPreset.MEDIUM
           })
         );
         this.renderPass.renderToScreen = false;
@@ -491,25 +489,8 @@ const Hyperspeed = ({ effectOptions = DEFAULT_EFFECT_OPTIONS }: any) => {
       loadAssets() {
         const assets = this.assets;
         return new Promise((resolve) => {
-          const manager = new THREE.LoadingManager(resolve as any);
-
-          const searchImage = new Image();
-          const areaImage = new Image();
-          assets.smaa = {};
-          searchImage.addEventListener('load', function () {
-            assets.smaa.search = this;
-            manager.itemEnd('smaa-search');
-          });
-
-          areaImage.addEventListener('load', function () {
-            assets.smaa.area = this;
-            manager.itemEnd('smaa-area');
-          });
-          manager.itemStart('smaa-search');
-          manager.itemStart('smaa-area');
-
-          searchImage.src = SMAAEffect.searchImageDataURL;
-          areaImage.src = SMAAEffect.areaImageDataURL;
+          // modern postprocessing doesn't need manual image URLs, but keep resolve
+          resolve(null);
         });
       }
 
